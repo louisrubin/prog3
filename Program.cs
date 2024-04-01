51992192b283b5b409916082bbac9bb3ff06309a
louisrubin <miguel10.rbn@gmail.com>
2024-04-01 12:49:15 -0300

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+Console.Write("ingrese mes: ");
+int numIngr = int.Parse(Console.ReadLine());
+Console.WriteLine("-------------");
+
+// PROGRAMA PARA OBTENER EL FACTORIAL DE UN NUMERO INGRESADO POR CONSOLA
+
+
+int factorial(int num)
+{
+    if (num == 1)
+    {
+        return num;
+    }
+    return num * factorial(num - 1);
+}
+Console.Write(factorial(numIngr));
