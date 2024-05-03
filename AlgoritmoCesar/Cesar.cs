9ae5bea348a337efee83bc9fd49b7aee898c5618
louisrubin <miguel10.rbn@gmail.com>
2024-05-03 01:20:47 -0300

    cifrado y descifrado

---
+++ AlgoritmoCesar/Cesar.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// RUBIN AZAS M. Luis - 2024
+
+namespace AlgoritmoCesar
+{
+    public static class Cesar
+    {
+        static char[] alfabeto = "aábcdeéfghiíjklmnñoópqrstuúüvwxyz".ToCharArray();  // alfabeto transformado a Array
+        
+        public static void Descifrar(string fraseCifrada, byte clave)
+        {
+            foreach (var item in fraseCifrada)
+            {
+                char caract = ' ';   // si no encuentra imprime un espacio
+                for (int i = 0; i < alfabeto.Length; i++)
+                {
+                    if (item == alfabeto[i])
+                    {
+                        int posicionEncontrada = Array.IndexOf(alfabeto, alfabeto[i]);
+                        int indiceRetrocedido = (posicionEncontrada - clave) % alfabeto.Length;
+
+                        if (indiceRetrocedido < 0) indiceRetrocedido += alfabeto.Length;
+
+                        caract = alfabeto[indiceRetrocedido];
+                        break;
+                    }
+                }
+                Console.Write(caract);
+            }
+        }
+
+        public static string Cifrar(string textoToCifrar, byte clave)
+        {
+            string fraseCifrada = "";
+
+            foreach (var item in textoToCifrar)
+            {
+                if (item == ' ')
+                {
+                    fraseCifrada += " ";
+                    continue;
+                }
+                for (int i = 0; i < alfabeto.Length; i++)
+                {
+                    if (alfabeto[i] == item)
+                    {
+                        int posicionEncontrada = Array.IndexOf(alfabeto, alfabeto[i]);
+                        int indiceAdelanto = (posicionEncontrada + clave) % alfabeto.Length;
+
+                        if (indiceAdelanto >= alfabeto.Length) indiceAdelanto -= alfabeto.Length;
+
+                        fraseCifrada += alfabeto[indiceAdelanto];
+                    }
+                }
+            }
+            //Console.WriteLine(fraseCifrada);
+            return fraseCifrada;
+        }
+    }
+}
---
+++ AlgoritmoCesar/Program.cs
@@ -0,0 +1,69 @@
+﻿using System.Xml.Linq;
+using AlgoritmoCesar;
+
+// RUBIN AZAS M. Luis - 2024
+
+string fraseCifrada = "kd wiqd géuúi tédbbúnq kdq wiqd iúígédíqsybyuqu";
+
+
+//Cesar.Cifrar("un gran poder conlleva una gran responsabilidad", 21);
+
+//if (fraseCifrada == cifrar("un gran poder conlleva una gran responsabilidad", 21))
+//{
+//    Console.WriteLine("son iguales");
+//}
+
+
+//                  MENU
+while (true)
+{
+    byte opc;
+    Console.Write("1- Descifrar\n2- Cifrar\n --> ");
+    byte.TryParse(Console.ReadLine(), out opc);
+
+    if (opc == 1)
+    {
+        descifrar();
+        break;
+    }
+    if (opc == 2) 
+    {
+        byte claveIngresada;
+        Console.Write("Ingrese frase: ");
+        string frase = Console.ReadLine();
+        Console.Write("Ingrese clave: ");
+        byte.TryParse(Console.ReadLine(), out claveIngresada);
+
+        Console.WriteLine("\nCifrado: " + Cesar.Cifrar(frase, claveIngresada));
+        break;
+    }
+    Console.WriteLine();
+}
+
+
+void descifrar()
+{
+    byte clave = 3;
+
+    while (true)
+    {
+        Cesar.Descifrar(fraseCifrada, clave);       // método estático Cesar.Descifrar
+
+        Console.Write("\nMensaje correcto? (si/no): ");
+        string resp = Console.ReadLine();
+        Console.WriteLine();
+
+        if (resp.ToLower().Equals("si"))
+        {
+            Console.WriteLine("La clave era: " + clave);
+            break;
+        }
+
+        clave += 3;
+    }
+}
+
+//string cifrar(string fraseToCifrar, byte clave)
+//{
+//    return Cesar.Cifrar(fraseToCifrar, clave);
+//}
