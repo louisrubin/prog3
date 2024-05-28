2888399c206594f166d019d8c9a6e8d76b48b35e
louisrubin <miguel10.rbn@gmail.com>
2024-05-28 13:37:38 -0300

    avanzando

--- Calculadora_WF/Boton.cs
+++ Calculadora_WF/Boton.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata.Ecma335;
 using System.Text;
 using System.Threading.Tasks;
 using static System.Runtime.InteropServices.JavaScript.JSType;
@@ -9,19 +10,28 @@ namespace Calculadora_WF
 {
     public abstract class Boton : Button
     {
+        //private static byte tableIndexStat = 0;
         public Boton(string text)
         {
             this.Text = text;
-            this.Size = new Size(80, 50);
-            this.BackColor = Color.Gray;
-            this.ForeColor = SystemColors.Window;
-            this.Font = new Font("Calibri", 18F, FontStyle.Bold);
+            this.Name = $"button{text}";
+            this.BackColor = Color.DimGray;
+            this.ForeColor = SystemColors.Control;
+            this.Font = new Font("Segoe UI", 20F, FontStyle.Bold);
             this.FlatAppearance.BorderSize = 0;
             this.FlatStyle = FlatStyle.Flat;
-            this.Margin = new Padding(0);
-            this.Name = $"button{text}";
-            //this.TabIndex = 9;
+
+            //this.TabIndex = tableIndexStat;     // indice tab
+
             this.UseVisualStyleBackColor = false;
+
+            this.Size = new Size(80, 50);           // tamaño actual
+            this.MaximumSize = new Size(90, 60);    // tamaño maximo
+            this.MinimumSize = new Size(80, 50);    // tamaño minimo
+            this.Margin = new Padding(0);
+
+            //tableIndexStat++;   // static +1
+
         }
     }
 }
--- Calculadora_WF/Form1.Designer.cs
+++ Calculadora_WF/Form1.Designer.cs
@@ -29,111 +29,191 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-
-            button1 = new Button();
-            button2 = new Button();
-            button3 = new Button();
-            button4 = new Button();
-            button5 = new Button();
-            button6 = new Button();
-            button7 = new Button();
-            button8 = new Button();
-            button9 = new Button();
-            button10 = new Button();
-
-
-            //button1 = new NumberButton(1);
-            //button2 = new NumberButton(2);
-            //button3 = new NumberButton(3);
-            //button4 = new NumberButton(4);
-            //button5 = new NumberButton(5);
-            //button6 = new NumberButton(6);
-            //button7 = new NumberButton(7);
-            //button8 = new NumberButton(8);
-            //button9 = new NumberButton(9);
-            //button10 = new NumberButton(0);
-
             SuspendLayout();
+
+            button1 = new NumberButton(1);
+            button2 = new NumberButton(2);
+            button3 = new NumberButton(3);
+            button4 = new NumberButton(4);
+            button5 = new NumberButton(5);
+            button6 = new NumberButton(6);
+            button7 = new NumberButton(7);
+            button8 = new NumberButton(8);
+            button9 = new NumberButton(9);
+            button10 = new NumberButton(0);
             // 
             // button1
             // 
-            button1.Location = new Point(3, 383);
+            button1.BackColor = Color.DimGray;
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.Font = new Font("Segoe UI", 20F, FontStyle.Bold);
+            button1.ForeColor = SystemColors.Control;
+            button1.Location = new Point(3, 395);
+            button1.Margin = new Padding(0);
+            button1.MaximumSize = new Size(90, 60);
+            button1.MinimumSize = new Size(80, 50);
             button1.Name = "button1";
-            button1.Size = new Size(75, 23);
+            button1.Numero = 1;
+            button1.Size = new Size(80, 50);
             button1.TabIndex = 9;
+            button1.Text = "1";
+            button1.UseVisualStyleBackColor = false;
             button1.Click += button1_Click;
             // 
             // button2
             // 
-            button2.Location = new Point(89, 383);
+            button2.BackColor = Color.DimGray;
+            button2.FlatStyle = FlatStyle.Flat;
+            button2.Font = new Font("Segoe UI", 20F, FontStyle.Bold);
+            button2.ForeColor = SystemColors.Control;
+            button2.Location = new Point(89, 395);
+            button2.Margin = new Padding(0);
+            button2.MaximumSize = new Size(90, 60);
+            button2.MinimumSize = new Size(80, 50);
             button2.Name = "button2";
-            button2.Size = new Size(75, 23);
+            button2.Numero = 2;
+            button2.Size = new Size(80, 50);
             button2.TabIndex = 8;
+            button2.Text = "2";
             button2.UseVisualStyleBackColor = true;
             // 
             // button3
             // 
-            button3.Location = new Point(175, 383);
+            button3.BackColor = Color.DimGray;
+            button3.FlatStyle = FlatStyle.Flat;
+            button3.Font = new Font("Segoe UI", 20F, FontStyle.Bold);
+            button3.ForeColor = SystemColors.Control;
+            button3.Location = new Point(175, 395);
+            button3.Margin = new Padding(0);
+            button3.MaximumSize = new Size(90, 60);
+            button3.MinimumSize = new Size(80, 50);
             button3.Name = "button3";
-            button3.Size = new Size(75, 23);
+            button3.Numero = 3;
+            button3.Size = new Size(80, 50);
             button3.TabIndex = 7;
+            button3.Text = "3";
             button3.UseVisualStyleBackColor = true;
             // 
             // button4
             // 
-            button4.Location = new Point(3, 286);
+            button4.BackColor = Color.DimGray;
+            button4.FlatStyle = FlatStyle.Flat;
+            button4.Font = new Font("Segoe UI", 20F, FontStyle.Bold);
+            button4.ForeColor = SystemColors.Control;
+            button4.Location = new Point(3, 342);
+            button4.Margin = new Padding(0);
+            button4.MaximumSize = new Size(90, 60);
+            button4.MinimumSize = new Size(80, 50);
             button4.Name = "button4";
-            button4.Size = new Size(75, 23);
+            button4.Numero = 4;
+            button4.Size = new Size(80, 50);
             button4.TabIndex = 6;
+            button4.Text = "4";
             button4.UseVisualStyleBackColor = true;
             // 
             // button5
             // 
-            button5.Location = new Point(89, 298);
+            button5.BackColor = Color.DimGray;
+            button5.FlatStyle = FlatStyle.Flat;
+            button5.Font = new Font("Segoe UI", 20F, FontStyle.Bold);
+            button5.ForeColor = SystemColors.Control;
+            button5.Location = new Point(89, 342);
+            button5.Margin = new Padding(0);
+            button5.MaximumSize = new Size(90, 60);
+            button5.MinimumSize = new Size(80, 50);
             button5.Name = "button5";
-            button5.Size = new Size(75, 23);
+            button5.Numero = 5;
+            button5.Size = new Size(80, 50);
             button5.TabIndex = 5;
+            button5.Text = "5";
             button5.UseVisualStyleBackColor = true;
             // 
             // button6
             // 
-            button6.Location = new Point(175, 322);
+            button6.BackColor = Color.DimGray;
+            button6.FlatStyle = FlatStyle.Flat;
+            button6.Font = new Font("Segoe UI", 20F, FontStyle.Bold);
+            button6.ForeColor = SystemColors.Control;
+            button6.Location = new Point(175, 342);
+            button6.Margin = new Padding(0);
+            button6.MaximumSize = new Size(90, 60);
+            button6.MinimumSize = new Size(80, 50);
             button6.Name = "button6";
-            button6.Size = new Size(75, 23);
+            button6.Numero = 6;
+            button6.Size = new Size(80, 50);
             button6.TabIndex = 4;
+            button6.Text = "6";
             button6.UseVisualStyleBackColor = true;
             // 
             // button7
             // 
-            button7.Location = new Point(3, 225);
+            button7.BackColor = Color.DimGray;
+            button7.FlatStyle = FlatStyle.Flat;
+            button7.Font = new Font("Segoe UI", 20F, FontStyle.Bold);
+            button7.ForeColor = SystemColors.Control;
+            button7.Location = new Point(3, 289);
+            button7.Margin = new Padding(0);
+            button7.MaximumSize = new Size(90, 60);
+            button7.MinimumSize = new Size(80, 50);
             button7.Name = "button7";
-            button7.Size = new Size(75, 23);
+            button7.Numero = 7;
+            button7.Size = new Size(80, 50);
             button7.TabIndex = 3;
+            button7.Text = "7";
             button7.UseVisualStyleBackColor = true;
             // 
             // button8
             // 
-            button8.Location = new Point(89, 261);
+            button8.BackColor = Color.DimGray;
+            button8.FlatStyle = FlatStyle.Flat;
+            button8.Font = new Font("Segoe UI", 20F, FontStyle.Bold);
+            button8.ForeColor = SystemColors.Control;
+            button8.Location = new Point(89, 289);
+            button8.Margin = new Padding(0);
+            button8.MaximumSize = new Size(90, 60);
+            button8.MinimumSize = new Size(80, 50);
             button8.Name = "button8";
-            button8.Size = new Size(75, 23);
+            button8.Numero = 8;
+            button8.Size = new Size(80, 50);
             button8.TabIndex = 2;
+            button8.Text = "8";
             button8.UseVisualStyleBackColor = true;
             // 
             // button9
             // 
-            button9.Location = new Point(175, 244);
+            button9.BackColor = Color.DimGray;
+            button9.FlatStyle = FlatStyle.Flat;
+            button9.Font = new Font("Segoe UI", 20F, FontStyle.Bold);
+            button9.ForeColor = SystemColors.Control;
+            button9.Location = new Point(175, 289);
+            button9.Margin = new Padding(0);
+            button9.MaximumSize = new Size(90, 60);
+            button9.MinimumSize = new Size(80, 50);
             button9.Name = "button9";
-            button9.Size = new Size(75, 23);
+            button9.Numero = 9;
+            button9.Size = new Size(80, 50);
             button9.TabIndex = 1;
+            button9.Text = "9";
             button9.UseVisualStyleBackColor = true;
             // 
             // button10
             // 
-            button10.Location = new Point(89, 442);
+            button10.BackColor = Color.DimGray;
+            button10.FlatStyle = FlatStyle.Flat;
+            button10.Font = new Font("Segoe UI", 20F, FontStyle.Bold);
+            button10.ForeColor = SystemColors.Control;
+            button10.Location = new Point(89, 448);
+            button10.Margin = new Padding(0);
+            button10.MaximumSize = new Size(90, 60);
+            button10.MinimumSize = new Size(80, 50);
             button10.Name = "button10";
-            button10.Size = new Size(75, 23);
+            button10.Numero = 0;
+            button10.Size = new Size(80, 50);
             button10.TabIndex = 0;
+            button10.Text = "0";
+            button10.UseVisualStyleBackColor = false;
             button10.Click += button10_Click_1;
+            
             // 
             // Form1
             // 
@@ -161,15 +241,15 @@
 
         #endregion
 
-        private Button button1;
-        private Button button2;
-        private Button button3;
-        private Button button4;
-        private Button button5;
-        private Button button6;
-        private Button button7;
-        private Button button8;
-        private Button button9;
-        private Button button10;
+        private NumberButton button1;
+        private NumberButton button2;
+        private NumberButton button3;
+        private NumberButton button4;
+        private NumberButton button5;
+        private NumberButton button6;
+        private NumberButton button7;
+        private NumberButton button8;
+        private NumberButton button9;
+        private NumberButton button10;
     }
 }
