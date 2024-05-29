184c999ab968eddc732878a28322e880cc6e1910
louisrubin <miguel10.rbn@gmail.com>
2024-05-29 16:53:32 -0300

    new textBox y config botones

--- Calculadora_WF/Form1.Designer.cs
+++ Calculadora_WF/Form1.Designer.cs
@@ -32,31 +32,34 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             tableLayoutBotones = new TableLayoutPanel();
-            button1 = new Boton();
-            button2 = new Boton();
-            button3 = new Boton();
-            button4 = new Boton();
-            button5 = new Boton();
+            button24 = new Boton();
+            button19 = new Boton();
+            button23 = new Boton();
+            button13 = new Boton();
+            button22 = new Boton();
+            button18 = new Boton();
+            button21 = new Boton();
             button6 = new Boton();
-            button7 = new Boton();
-            button8 = new Boton();
+            button20 = new Boton();
+            button17 = new Boton();
+            button12 = new Boton();
+            button16 = new Boton();
+            button1 = new Boton();
+            button15 = new Boton();
+            button5 = new Boton();
+            button2 = new Boton();
             button9 = new Boton();
+            button4 = new Boton();
+            button8 = new Boton();
+            button3 = new Boton();
+            button7 = new Boton();
             button10 = new Boton();
             button11 = new Boton();
-            button12 = new Boton();
-            button13 = new Boton();
             button14 = new Boton();
-            button15 = new Boton();
-            button16 = new Boton();
-            button17 = new Boton();
-            button18 = new Boton();
-            button19 = new Boton();
-            button20 = new Boton();
-            button21 = new Boton();
-            button22 = new Boton();
-            button23 = new Boton();
-            button24 = new Boton();
+            tableLayoutPanel1 = new TableLayoutPanel();
+            textBox1 = new TextBox();
             tableLayoutBotones.SuspendLayout();
+            tableLayoutPanel1.SuspendLayout();
             SuspendLayout();
             // 
             // tableLayoutBotones
@@ -109,53 +112,62 @@
             //button1.Location = new Point(5, 209);
             button1.Name = "button1";
             button1.Text = "1";
+            button1.Click += button1_Click;
             // 
             // button2
             // 
-            
+
             button2.Name = "button2";
             button2.Text = "2";
-
+            button2.Click += button2_Click;
             // 
             // button3
             // 
             button3.Name = "button3";
             button3.Text = "3";
+            button3.Click += button3_Click;
             // 
             // button4
             // 
             button4.Name = "button4";
             button4.Text = "4";
+            button4.Click += button4_Click;
             // 
             // button5
             // 
             button5.Name = "button5";
             button5.Text = "5";
+            button5.Click += button5_Click;
             // 
             // button6
             // 
             button6.Name = "button6";
             button6.Text = "6";
+            button6.Click += button6_Click;
             // 
             // button7
             // 
             button7.Name = "button7";
             button7.Text = "7";
+            button7.Click += button7_Click;
             // 
             // button8
             // 
             button8.Name = "button8";
             button8.Text = "8";
+            button8.Click += button8_Click;
             // 
             // button9
             // 
             button9.Name = "button9";
             button9.Text = "9";
+            button9.Click += button9_Click;
             // 
             // button10
             // 
             button10.Name = "button10";
             button10.Text = "0";
+            button10.Click += button10_Click;
             // 
             // button11
             // 
@@ -197,21 +209,25 @@
             // 
             button18.Name = "button18";
             button18.Image = Properties.Resources.divide_icon;
+            button18.Click += button18_Click;
             // 
             // button19
             // 
             button19.Name = "button19";
             button19.Image = Properties.Resources.multiplication_icon2;
+            button19.Click += button19_Click;
             // 
             // button20
             // 
             button20.Name = "button20";
             button20.Image = Properties.Resources.substract_icon2;
+            button20.Click += button20_Click;
             // 
             // button21
             // 
             button21.Name = "button21";
             button21.Image = Properties.Resources.plus_sign2;
+            button21.Click += button21_Click;
             // 
             // button22
             // 
@@ -222,12 +238,44 @@
             // 
             button23.Name = "button23";
             button23.Text = ",";
+            button23.Click += button23_Click;
             // 
             // button24
             // 
             button24.Name = "button24";
             button24.Image = Properties.Resources.plus_minus_icon;
-
+            // 
+            // tableLayoutPanel1
+            // 
+            tableLayoutPanel1.ColumnCount = 1;
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 100F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 20F));
+            tableLayoutPanel1.Controls.Add(textBox1, 0, 0);
+            tableLayoutPanel1.Dock = DockStyle.Top;
+            tableLayoutPanel1.Location = new Point(0, 0);
+            tableLayoutPanel1.Name = "tableLayoutPanel1";
+            tableLayoutPanel1.Padding = new Padding(0, 30, 0, 0);
+            tableLayoutPanel1.RowCount = 1;
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 100F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 20F));
+            tableLayoutPanel1.Size = new Size(344, 114);
+            tableLayoutPanel1.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            textBox1.BackColor = Color.FromArgb(64, 64, 64);
+            textBox1.BorderStyle = BorderStyle.None;
+            textBox1.Dock = DockStyle.Bottom;
+            textBox1.Font = new Font("Segoe UI", 35F, FontStyle.Bold);
+            textBox1.ForeColor = SystemColors.Control;
+            textBox1.Location = new Point(12, 48);
+            textBox1.Margin = new Padding(12, 3, 12, 3);
+            textBox1.Name = "textBox1";
+            textBox1.ReadOnly = true;
+            textBox1.Size = new Size(320, 63);
+            textBox1.TabIndex = 0;
+            textBox1.Text = "0";
+            textBox1.TextAlign = HorizontalAlignment.Right;
             // 
             // Form1
             // 
@@ -235,13 +283,17 @@
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.FromArgb(64, 64, 64);
             ClientSize = new Size(344, 501);
+            Controls.Add(tableLayoutPanel1);
             Controls.Add(tableLayoutBotones);
             Icon = (Icon)resources.GetObject("$this.Icon");
             MaximumSize = new Size(460, 640);
             MinimumSize = new Size(360, 540);
             Name = "Form1";
             Text = "Calculadora";
+            Resize += Form1_Resize;
             tableLayoutBotones.ResumeLayout(false);
+            tableLayoutPanel1.ResumeLayout(false);
+            tableLayoutPanel1.PerformLayout();
             ResumeLayout(false);
         }
 
@@ -272,5 +324,7 @@
         private Boton button10;
         private Boton button11;
         private Boton button14;
+        private TableLayoutPanel tableLayoutPanel1;
+        private TextBox textBox1;
     }
 }
--- Calculadora_WF/Form1.cs
+++ Calculadora_WF/Form1.cs
@@ -2,25 +2,113 @@ namespace Calculadora_WF
 {
     public partial class Form1 : Form
     {
+        private string listOperac = "";
         public Form1()
         {
             InitializeComponent();
-            
+
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-
+            listOperac += "1";
+            textBox1.Text = listOperac;
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listOperac += "2";
+            textBox1.Text = listOperac;
         }
 
+        private void button3_Click(object sender, EventArgs e)
+        {
+            listOperac += "3";
+            textBox1.Text = listOperac;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            listOperac += "4";
+            textBox1.Text = listOperac;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            listOperac += "5";
+            textBox1.Text = listOperac;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            listOperac += "6";
+            textBox1.Text = listOperac;
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            listOperac += "7";
+            textBox1.Text = listOperac;
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            listOperac += "8";
+            textBox1.Text = listOperac;
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            listOperac += "9";
+            textBox1.Text = listOperac;
+        }
         private void button10_Click(object sender, EventArgs e)
         {
-
+            listOperac += "0";
+            textBox1.Text = listOperac;
         }
 
-        private void button10_Click_1(object sender, EventArgs e)
+        private void Form1_Resize(object sender, EventArgs e)
         {
-
+            if (this.WindowState == FormWindowState.Maximized)
+            {
+                // Establecer el tamaño deseado (por ejemplo, 800x600)
+                //this.WindowState = FormWindowState.Normal;
+                this.Size = new Size(360, 540);
+                //this.Location = new Point((Screen.PrimaryScreen.WorkingArea.Width - this.Width) / 2,
+                //                          (Screen.PrimaryScreen.WorkingArea.Height - this.Height) / 2);
+            }
         }
+
+        private void button21_Click(object sender, EventArgs e)
+        {
+            listOperac += "+";
+            textBox1.Text = listOperac;
+        }
+
+        private void button20_Click(object sender, EventArgs e)
+        {
+            listOperac += "-";
+            textBox1.Text = listOperac;
+        }
+
+        private void button19_Click(object sender, EventArgs e)
+        {
+            listOperac += "x";
+            textBox1.Text = listOperac;
+        }
+
+        private void button18_Click(object sender, EventArgs e)
+        {
+            listOperac += "/";
+            textBox1.Text = listOperac;
+        }
+
+        private void button23_Click(object sender, EventArgs e)
+        {
+            listOperac += ",";
+            textBox1.Text = listOperac;
+        }
+
+        
     }
 }
